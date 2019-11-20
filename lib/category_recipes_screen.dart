928b920ced31898be04d70d996df00fcd026ae95
arcediazadrian <arce.diaz.adrian@gmail.com>
2019-11-20 18:47:53 -0400

    Initial commit

---
+++ lib/category_recipes_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryRecipesScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryRecipesScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: Center(
+        child: Text('The recipes for the category'),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/category_recipes_screen.dart';
+
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(25, 51, 51, 1)),
+              body2: TextStyle(color: Color.fromRGBO(25, 51, 51, 1)),
+              title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold),
+            ),
+      ),
+      home: CategoriesScreen(),
+      routes: {
+        '/category-recipes': (ctx) => CategoryRecipesScreen()
+      },
+    );
+  }
+}
